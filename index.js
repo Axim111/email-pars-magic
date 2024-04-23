1322e3118da0d9df62e628938ea81485f2be05cc
Axim <makseropolov@gmail.com>
2024-04-23 19:21:54 +0500

    iteral

--- index.js
+++ index.js
@@ -1,45 +1,33 @@
-import puppeteer from 'puppeteer-extra';
+import puppeteer from 'puppeteer';
 
-puppeteer.use()
+
 
 function waitFor(delay) {
   return new Promise(resolve => setTimeout(resolve, delay));
 }
+try {
 
-let main = (async () => {
-  // Launch the browser and open a new blank page
-  const browser = await puppeteer.launch({
-    headless: false,
-    ignoreHTTPSErrors: true,
-    defaultViewport: false,
-  });
-
-
-  const page = await browser.newPage();
-  await page.goto("https://1secmail.ru/ru", { waitUntil: "load" })
-
-  let inpu = ""
-  let alred = ""
-  try {
-    while (!inpu) {
-
-      waitFor(3000)
-      if ((typeof (alred) == "string"))
-        inpu = await (await page.waitForSelector(".custom-email-input"))
-          .evaluate((node) => node.innerText)
-
-      alred = await (await page.$(".custom-email-botton"))
-        .evaluate((node) => node.getAttribute("disabled"))
-      console.log(typeof (alred), typeof (inpu))
-
+  var main = (async () => {
+    // Launch the browser and open a new blank page
+    const browser = await puppeteer.launch({
+      headless: false,
+      ignoreHTTPSErrors: true,
+      defaultViewport: false,
+    });
+    const page = await browser.newPage();
+    let mass = ["https://kwork.ru/projects", "https://1secmail.ru/ru", "https://vk.com/feed"]
+    for (let item of mass) {
+      console.log(item)
+      await page.goto(item, { waitUntil: "load" })
+      console.log(await page.content());
     }
 
-  }
 
-  catch (er) {
-    console.error(er)
-  }
-  console.log(await inpu)
 
-});
-main();
+  })
+}
+catch (err) {
+  console.log(err)
+}
+
+main()
